fc9de836f96ef3902ecc586659b4862b151ae2ca
Vit Benton <viteksk8alot@gmail.com>
2018-04-15 11:55:49 -0400

    Complete base styling on homepage

--- src/js/home.js
+++ src/js/home.js
@@ -2,4 +2,12 @@ $(document).ready(() => {
 
     alert('Welcome to Node.js!');
 
+    $('.node-logo').mousedown(() => {
+        $('body').toggleClass('nodeGreen');
+    });
+
+    $('.node-logo').mouseup(() => {
+        $('body').toggleClass('nodeGreen');
+    });
+
 });
